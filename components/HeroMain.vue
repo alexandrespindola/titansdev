343e6556cf515c2ed79d6e3e023a198cd1ab92e7
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-15 09:29:21 +0200

    Test

--- components/HeroMain.vue
+++ components/HeroMain.vue
@@ -6,7 +6,7 @@
           <div>
             <div class="flex flex-row">
               <div class="w-full flex flex-col text-center items-center gap-4">
-                <h1 class="text-4xl font-semibold uppercase md:text-4xl lg:text-6xl">Tecnologías modernas para su negocio
+                <h1 class="text-4xl font-semibold uppercase md:text-4xl lg:text-6xl">Tecnologías mmodernas para su negocio
                 </h1>
                 <h2 class="flex flex-row text-2xl font-semibold">Soluciones para acelerar el crecimiento de pequeñas y medianas
                   empresas</h2>
