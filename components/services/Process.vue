a9b6cc9098f5dd200eacd0a941471cced4bc9780
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-19 18:38:41 +0200

    Atualiza referências de tipos no Nuxt; melhora a formatação do componente Process e ajusta a lógica de carregamento de posts

--- components/services/Process.vue
+++ components/services/Process.vue
@@ -16,8 +16,7 @@
           </div>
           <h5 class="mb-15">1. Análisis</h5>
           <p>
-            Realizamos un estudio de su negocio, objetivos y público objetivo. Definimos la estrategia digital adecuada,
-            ya sea para aumentar la visibilidad en línea o para una transformación digital integral.
+            Realizamos un estudio de su negocio, objetivos y público objetivo. Definimos la estrategia digital adecuada, ya sea para aumentar la visibilidad en línea o para una transformación digital integral.
           </p>
         </div>
         <div class="col-lg-3 col-md-6 item md-mb50">
@@ -27,8 +26,7 @@
           <h5 class="mb-15">2. Desarrollo
           </h5>
           <p>
-            Creamos soluciones a medida, desde páginas web profesionales hasta sistemas web y apps móviles. Aplicamos
-            las mejores prácticas de UX/UI y optimización para buscadores.
+            Creamos soluciones a medida, desde páginas web profesionales hasta sistemas web y apps móviles. Aplicamos las mejores prácticas de UX/UI y optimización para buscadores.
           </p>
         </div>
         <div class="col-lg-3 col-md-6 item sm-mb50">
@@ -38,8 +36,7 @@
           <h5 class="mb-15">3. Implementación
           </h5>
           <p>
-            Ponemos en marcha las soluciones desarrolladas, configuramos herramientas de marketing digital, servidores y
-            automatizamos procesos. Realizamos pruebas y ajustes para garantizar un rendimiento óptimo.
+            Ponemos en marcha las soluciones desarrolladas, configuramos herramientas de marketing digital, servidores y automatizamos procesos. Realizamos pruebas y ajustes para garantizar un rendimiento óptimo.
           </p>
         </div>
         <div class="col-lg-3 col-md-6 item">
@@ -48,23 +45,22 @@
           </div>
           <h5 class="mb-15">4. Mantenimiento</h5>
           <p>
-            Proporcionamos soporte y mantenimiento de los sitios web y sistemas implementados. Nos aseguramos de que
-            todas las plataformas estén actualizadas, seguras y funcionando de manera eficiente.
+            Proporcionamos soporte y mantenimiento de los sitios web y sistemas implementados. Nos aseguramos de que todas las plataformas estén actualizadas, seguras y funcionando de manera eficiente.
           </p>
         </div>
       </div>
     </div>
     <div>
-      <h1>Posts</h1>
-      <p v-if="error">Erro ao carregar posts: {{ error.message }}</p>
-      <ul v-else-if="posts && posts.data">
-        <li v-for="post in posts.data" :key="post.id">
-          {{ post.title }}
-          {{ post.subtitle }}
-        </li>
-      </ul>
-      <p v-else>Carregando posts...</p>
-    </div>
+    <h1>Posts</h1>
+    <p v-if="error">Erro ao carregar posts: {{ error.message }}</p>
+    <ul v-else-if="posts && posts.data">
+      <li v-for="post in posts.data" :key="post.id">
+        {{ post.title }}
+        {{ post.subtitle }}
+      </li>
+    </ul>
+    <p v-else>Carregando posts...</p>
+  </div>
   </section>
 </template>
 
@@ -72,10 +68,10 @@
 
 const config = useRuntimeConfig()
 
-const { data: posts, error } = await useAsyncData('posts', () => $fetch(`${config.public.strapiUri}/api/posts`, {
+const { data: posts, error } = await useFetch(`${config.public.strapiUri}/api/posts`, {
   headers: {
     'Authorization': `Bearer ${config.strapiApiToken}`
   }
 })
-)
+
 </script>
