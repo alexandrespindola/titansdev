2b5185c4971005ea36f2277f4ce9a7bc0a5bcb7f
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-23 17:40:48 +0200

    Adiciona componente "CheckAnimation" e integra na página "Gracias"; atualiza referências de tipos no Nuxt

---
+++ components/common/CheckAnimation.vue.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition name="fade" appear>
+    <div v-if="show" class="check-animation">
+      <i class="fas fa-check"></i>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+const show = ref(false);
+
+const triggerAnimation = () => {
+  show.value = true;
+};
+
+onMounted(() => {
+  // Usar nextTick para garantir que a animação seja aplicada após a renderização
+  nextTick(() => {
+    triggerAnimation();
+  });
+});
+</script>
+
+<style lang="scss" scoped>
+.check-animation {
+  color: green;
+  font-size: 4em;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.30s ease-out;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
