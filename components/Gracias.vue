9b134254e1641043af476f6e9bbca534b9f6ec96
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-23 15:30:36 +0200

    Adiciona componente "Gracias" e página correspondente; atualiza definições de tipos e referências no Nuxt

---
+++ components/Gracias.vue
@@ -0,0 +1,90 @@
+<template>
+  <header class="header page-header-error valign">
+    <div class="container">
+      <div class="text-center">
+        <div class="row justify-content-center">
+          <div class="col-lg-5">
+                        <div class="text mt-40">
+              <h2 class="mb-10">¡Gracias!</h2>
+              <p>Su mensaje se ha enviado correctamente y uno de nuestros representantes se pondrá en contacto con usted en breve.</p>
+              <NuxtLink
+                to="/"
+                class="butn butn-md butn-bg main-colorbg text-dark radius-30 mt-30"
+              >
+                <span class="text">Volver al Inicio</span>
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-marq">
+      <div class="slide-har st1">
+        <div class="box">
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center fw-800">
+              <span>Gracias</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ pages/gracias.vue
@@ -0,0 +1,34 @@
+<template>
+  <ProgressScroll />
+  <Lines />
+  <div id="smooth-wrapper">
+    <Navbar />
+    <div id="smooth-content">
+      <main class="main-bg">
+        <Gracias />
+      </main>
+      <FooterPrimary />
+    </div>
+  </div>
+</template>
+<script setup>
+
+useHead({
+  title: 'Gracias!',
+  
+  link: [
+    { rel: 'stylesheet', href: '/dark/assets/css/plugins.css' },
+    { rel: 'stylesheet', href: '/dark/assets/css/satoshi.css' },
+    { rel: 'stylesheet', href: '/dark/assets/css/style.css' },
+  ],
+});
+
+onMounted(() => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  ScrollTrigger.normalizeScroll(true);
+  ScrollSmoother.create({
+    smooth: 2,
+    effects: true,
+  });
+});
+</script>
