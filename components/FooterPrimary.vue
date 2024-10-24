4fe00541b4a6a20dc37a16840dabbb3c3a6287bf
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-24 18:02:56 +0200

    Atualiza o layout do rodapé: ajusta larguras de colunas, centraliza texto e substitui links por políticas de privacidade, termos de uso e política de cookies. Adiciona estilo SCSS para o rodapé.

--- components/FooterPrimary.vue
+++ components/FooterPrimary.vue
@@ -2,30 +2,39 @@
   <footer class="lg:pt-30 pb-50">
     <div class="container">
       <div class="flex flex-col flex-wrap gap-12 lg:gap-0 lg:flex-row">
-        <div class="w-full order-first flex flex-col items-center lg:w-[35%] lg:flex-row lg:justify-start">
+        <div class="w-full order-first flex flex-col items-center lg:w-[40%] lg:flex-row lg:justify-start">
           <NuxtLink to="/">
             <img src="/dark/assets/imgs/titansdev-logo-white.webp" alt="" class="object-contain w-[120px] h-auto">
           </NuxtLink>
         </div>
-        <div class=" w-full flex flex-wrap justify-center order-last lg:w-[30%] lg:order-2">
+        <div class=" w-full flex flex-wrap justify-center items-center content-center order-last lg:w-[20%] lg:order-2">
           <p class="fz-14 w-full flex justify-center gap-1">
             <NuxtLink to="/" class="main-color">
               TitansDev
             </NuxtLink>
             <span>© 2024</span>
           </p>
-          <p class="fz-14 self-center">
+          <p class="fz-14 self-center text-center">
             Producido con ❤️ por TitansDev
           </p>
         </div>
-        <div class="w-full lg:w-[35%] lg:order-last">
-          <ul class="flex flex-col gap-4 lg:gap-8 lg:flex-row lg:justify-end">
-            <NuxtLink to="/quienes-somos" class="self-center">Quiénes Somos</NuxtLink>
-            <NuxtLink to="/servicios" class="self-center">Servicios</NuxtLink>
-            <NuxtLink to="/contacto" class="self-center">Contacto</NuxtLink>
+        <div class="w-full lg:w-[40%] lg:order-last">
+          <ul class="footer-primary flex flex-col gap-2 xl:flex-row xl:justify-end">
+            <NuxtLink to="/politica-de-privacidad" class="self-center">Política de Privacidad</NuxtLink>
+            <NuxtLink to="/terminos-de-uso" class="self-center">Términos de Uso</NuxtLink>
+            <NuxtLink to="/politica-de-cookies" class="self-center">Política de Coookies</NuxtLink>
           </ul>
         </div>
       </div>
     </div>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.footer-primary {
+  @media screen and (min-width: 1280px) {
+    @apply gap-8 !important;
+
+  }
+}
+</style>
