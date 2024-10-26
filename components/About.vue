aa780a40d4eb849992f5db188519a952447a2001
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-26 10:22:07 +0200

    Refatora componente About para melhorar a estrutura e responsividade. Ajusta espaçamento e otimiza a legibilidade do texto.

--- components/About.vue
+++ components/About.vue
@@ -8,52 +8,39 @@
             <h3>
               Déjenos hacer el trabajo con la tecnología, para que tú puedas centrarse en lo importante de tu negócio.
             </h3>
-            <div class="d-flex align-items-end mt-4">
-              <div>
+            <div class="d-flex align-items-end mt-4 gap-x-8">
+              <div class="hidden sm:block">
                 <NuxtLink to="/quienes-somos" class="d-flex align-items-end">
                   <span class="sub-title nowrap">Sobre Nosotros</span>
                   <span class="fz-70 line-height-45 ti-arrow-top-right"></span>
                 </NuxtLink>
               </div>
-              <div class="ms-4">
-                <p>
-                  Sabemos que su tiempo es valioso y que debe centrarse en hacer crecer su negocio. Por eso nos ocupamos desde la creación y automatización de sitios web hasta la generación de clientes potenciales cualificados.
-                </p>
-              </div>
+              <p>
+                Sabemos que su tiempo es valioso y que debe centrarse en hacer crecer su negocio. Por eso nos ocupamos
+                desde la creación y automatización de sitios web hasta la generación de clientes potenciales
+                cualificados.
+              </p>
             </div>
+
           </div>
         </div>
         <div class="w-full lg:w-2/3 xl:w-[48%]">
           <div class="img-vid">
             <img src="/dark/assets/imgs/intro/intro-touchscreen-plant.webp" alt="" />
             <div class="curv-butn main-bg">
-                <div class="icon">
-                  <i class="fas fa-play"></i>
-                </div>
+              <div class="icon">
+                <i class="fas fa-play"></i>
+              </div>
               <div class="shap-left-top">
-                <svg
-                  viewBox="0 0 11 11"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-11 h-11"
-                >
-                  <path
-                    d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z"
-                    fill="#1a1a1a"
-                  ></path>
+                <svg viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-11 h-11">
+                  <path d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z"
+                    fill="#1a1a1a"></path>
                 </svg>
               </div>
               <div class="shap-right-bottom">
-                <svg
-                  viewBox="0 0 11 11"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-11 h-11"
-                >
-                  <path
-                    d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z"
-                    fill="#1a1a1a"
-                  ></path>
+                <svg viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-11 h-11">
+                  <path d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z"
+                    fill="#1a1a1a"></path>
                 </svg>
               </div>
             </div>
