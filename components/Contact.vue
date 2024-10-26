8ee3b8d92aca12bcaa85c91dc38ee33a5de4956c
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-10-26 09:54:09 +0200

    Atualiza referências de tipos no nuxt.d.ts e ajusta estilo do componente Contact para melhorar a responsividade e legibilidade.

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="contact pt-12 pb-24" data-scroll-index="6">
+  <section class="contact pt-12 pb-24">
     <div class="container">
-      <div class="flex flex-col md:flex-row md:gap-4">
-        <div class="w-full valign md:w-2/5">
+      <div class="flex flex-col w-full lg:flex-row gap-x-8">
+        <div class="w-full valign lg:w-2/5">
           <div class="sec-head info-box full-width md-mb80">
             <NuxtLink to="https://wa.me/+34642887239" target="_blank">
-              <div class="phone fz-30 fw-600 main-color flex flex-row gap-3 items-center">
+              <div class="phone text-2xl lg:text-3xl fw-600 main-color flex flex-row gap-3 items-center">
                 <UIcon name="logos:whatsapp-icon" />
                 +34 642 887 239
               </div>
@@ -19,7 +19,6 @@
               <h6 class="mb-15">Email</h6>
               <p>contacto@titansdev.es</p>
             </div>
-
             <div class="social-icon-circle mt-50 flex flex-row gap-x-3">
               <NuxtLink to="/" target="blank">
                 <i class="fab fa-facebook"></i>
@@ -30,7 +29,7 @@
             </div>
           </div>
         </div>
-        <div class="w-full offset-lg-1 valign md:w-3/5">
+        <div class="w-full valign lg:w-3/5">
           <div class="full-width">
             <div class="sec-head mb-50">
               <h6 class="sub-title main-color mb-15">Vamos a hablar</h6>
--- components/services/details/Services.vue
+++ components/services/details/Services.vue
@@ -1,36 +1,29 @@
 <template>
-  <section class="services-details section-padding">
+  <section class="services-details pt-[7rem] pb-[3rem]">
     <div class="container">
       <div class="sec-head text-center mb-80">
         <div class="row justify-content-center">
-          <div class="col-lg-8">
-            <div>
-              <h6 class="sub-title main-color mb-15">
-                Desarrollo de Páginas Web & Landing Pages
-              </h6>
-              <h4>
-                Even the all-powerful Pointing has no control about the blind
-                texts it is an almost unorthographic life One day however a
-                small line of blind text.
-              </h4>
-            </div>
+          <div>
+            <h6 class="sub-title main-color mb-15">
+              Desarrollo de Páginas Web & Landing Pages
+            </h6>
+            <h4>
+              Impulsa tu negocio con un sitio web que atrae clientes y genera resultados.
+            </h4>
           </div>
         </div>
       </div>
-      <div class="row serv-imgs mt-80">
-        <div class="col-lg-4">
-          <div class="img o-hidden radius-15 fit-img md-mb30">
-            <img src="/dark/assets/imgs/intro/01.jpg" alt="" />
+      <div class="flex flex-col md:flex-row gap-8">
+        <div class="w-full md:w-1/3">
+          <div class="relative w-full pt-[100%] overflow-hidden">
+            <img src="/dark/assets/imgs/intro/01.jpg" alt=""
+              class="absolute top-0 left-0 w-full h-full object-cover rounded-3xl" />
           </div>
         </div>
-        <div class="col-lg-8">
-          <div class="img o-hidden radius-15">
-            <img
-              src="/dark/assets/imgs/background/web-development.webp"
-              alt=""
-              data-speed="auto"
-              data-lag="0"
-            />
+        <div class="w-full md:w-2/3">
+          <div class="relative w-full pt-[100%] md:pt-[50%] overflow-hidden">
+            <img src="/dark/assets/imgs/background/web-development.webp" alt=""
+              class="absolute top-0 left-0 w-full h-full object-cover rounded-3xl" />
           </div>
         </div>
       </div>
@@ -38,75 +31,48 @@
         <div class="col-lg-4">
           <div class="text md-mb50">
             <p>
-              Even the all-powerful Pointing has no control about the blind
-              texts it is an almost unorthographic life One day however a small
-              line of blind text by the name of Lorem Ipsum decided to leave for
-              the far World of Grammar.
+              Si eres una pequeña o mediana empresa, o un profesional autónomo, sabes lo importante que es tener una buena presencia en línea.
+              Nosotros entendemos eso y creamos sitios web que no solo presentan tus productos y servicios, sino que también se conectan eficazmente con tus clientes. Con un diseño bonito y fácil de usar, tu sitio será una poderosa herramienta para conquistar
+              nuevos negocios y fortalecer tu marca.
             </p>
           </div>
         </div>
         <div class="col-lg-8">
-          <div class="row">
-            <div class="col-md-6">
+          <div class="flex flex-col md:flex-row flex-wrap gap-y-2">
+            <h4 class="w-full mb-4">Ventajas de un sitio web de calidad, rápido y responsivo</h4>
+            <div class="col-md-6">   
               <ul class="rest list-arrow">
                 <li class="nowrap">
                   <span class="icon">
-                    <svg
-                      width="100%"
-                      height="100%"
-                      viewBox="0 0 9 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
+                    <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"
-                      ></path>
+                        fill="#40ea1e"></path>
                     </svg>
                   </span>
-                  <h6 class="inline fw-400">Amazing communication.</h6>
+                  <h6 class="inline fw-400">Atrae Más Clientes</h6>
                 </li>
                 <li class="mt-10 nowrap">
                   <span class="icon">
-                    <svg
-                      width="100%"
-                      height="100%"
-                      viewBox="0 0 9 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
+                    <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"
-                      ></path>
+                        fill="#40ea1e"></path>
                     </svg>
                   </span>
                   <h6 class="inline fw-400">
-                    Best trendinf designing experience.
+                    Facilidad de Uso
                   </h6>
                 </li>
                 <li class="mt-10 nowrap">
                   <span class="icon">
-                    <svg
-                      width="100%"
-                      height="100%"
-                      viewBox="0 0 9 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
+                    <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"
-                      ></path>
+                        fill="#40ea1e"></path>
                     </svg>
                   </span>
-                  <h6 class="inline fw-400">Email & Live chat.</h6>
+                  <h6 class="inline fw-400">Valora tu Negócio</h6>
                 </li>
               </ul>
             </div>
@@ -114,62 +80,35 @@
               <ul class="rest list-arrow">
                 <li class="nowrap">
                   <span class="icon">
-                    <svg
-                      width="100%"
-                      height="100%"
-                      viewBox="0 0 9 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
+                    <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"
-                      ></path>
+                        fill="#40ea1e"></path>
                     </svg>
                   </span>
-                  <h6 class="inline fw-400">Amazing communication.</h6>
+                  <h6 class="inline fw-400">Aumenta tus Ventas</h6>
                 </li>
                 <li class="mt-10 nowrap">
                   <span class="icon">
-                    <svg
-                      width="100%"
-                      height="100%"
-                      viewBox="0 0 9 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
+                    <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"
-                      ></path>
+                        fill="#40ea1e"></path>
                     </svg>
                   </span>
                   <h6 class="inline fw-400">
-                    Best trendinf designing experience.
+                    Soporte Amigable
                   </h6>
                 </li>
                 <li class="mt-10 nowrap">
                   <span class="icon">
-                    <svg
-                      width="100%"
-                      height="100%"
-                      viewBox="0 0 9 8"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
+                    <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"
-                      ></path>
+                        fill="#40ea1e"></path>
                     </svg>
                   </span>
-                  <h6 class="inline fw-400">Email & Live chat.</h6>
+                  <h6 class="inline fw-400">Resultados Visibles</h6>
                 </li>
               </ul>
             </div>
