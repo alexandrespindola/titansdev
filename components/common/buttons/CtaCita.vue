1102f3ea7d7f751fedfff7b16f328aa3ffaab492
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-11-04 15:13:28 +0100

    Atualiza o componente de preços com novos pacotes e descrições; ajusta o botão de agendamento para melhor formatação

--- components/common/buttons/CtaCita.vue
+++ components/common/buttons/CtaCita.vue
@@ -1,7 +1,9 @@
-<template>          <div class="flex flex-row">
-  <UButton class="button mt-4 flex flex-row items-center gap-2 align-center self-center min-h-12" to="https://calcom.titansdev.es/alexandre/cita" target="_blank">
-    Agendar Cita Online
-    <UIcon name="hugeicons:cursor-pointer-02" class="w-6 h-6 text-white"/>
-  </UButton>
-</div>
+<template>
+  <div class="flex flex-row">
+    <UButton class="button mt-4 flex flex-row items-center gap-2 align-center self-center min-h-12"
+      to="https://calcom.titansdev.es/alexandre/cita" target="_blank">
+      Agendar Cita Online
+      <UIcon name="hugeicons:cursor-pointer-02" class="w-6 h-6 text-white" />
+    </UButton>
+  </div>
 </template>
--- components/services/details/Pricing.vue
+++ components/services/details/Pricing.vue
@@ -2,32 +2,44 @@
   <section class="process">
     <div class="relative isolate px-6 py-24 sm:py-32 lg:px-8">
       <div class="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl" aria-hidden="true">
-        <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#2e9c18] to-[#40ea1e] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+        <div class="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#2e9c18] to-[#40ea1e] opacity-30"
+          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
       </div>
       <div class="mx-auto max-w-4xl text-center">
         <h2 class="sub-title text-[#40ea1e]">Precios</h2>
-        <h3 class="mt-2 text-balance text-5xl font-semibold tracking-tight text-white sm:text-6xl">Choose the right plan for you</h3>
+        <h3 class="mt-2 text-balance text-5xl font-semibold tracking-tight text-white sm:text-6xl">Elija el mejor plan para usted</h3>
       </div>
-      <p class="mx-auto mt-6 max-w-2xl text-pretty text-center">Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer loyalty, and driving sales.</p>
-      <div class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-        <div v-for="(tier, tierIdx) in tiers" :key="tier.id" :class="[tier.featured ? 'relative bg-black shadow-2xl' : 'bg-white sm:mx-8 lg:mx-0', tier.featured ? '' : tierIdx === 0 ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-bl-3xl lg:rounded-tr-none' : 'sm:rounded-t-none lg:rounded-bl-none lg:rounded-tr-3xl', 'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10']">
-          <h3 :id="tier.id" :class="[tier.featured ? 'text-[#40ea1e]' : 'text-[#40ea1e]', 'text-base/7 font-semibold']">{{ tier.name }}</h3>
+      <p class="mx-auto mt-6 max-w-2xl text-pretty text-center">Elija un plan asequible con las mejores funciones para atraer a su público, fidelizar a sus clientes e impulsar las ventas.</p>
+      <!-- <div class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2"> -->
+      <div
+        class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-7xl lg:grid-cols-3">
+        <div v-for="(tier, tierIdx) in tiers" :key="tier.id"
+          :class="[tier.featured ? 'relative bg-black shadow-2xl' : 'bg-white sm:mx-8 lg:mx-0', tier.featured ? '' : tierIdx === 0 ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-bl-3xl lg:rounded-tr-none' : 'sm:rounded-t-none lg:rounded-bl-none lg:rounded-tr-3xl', 'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10']">
+          <h3 :id="tier.id" :class="[tier.featured ? 'text-[#40ea1e]' : 'text-[#40ea1e]', 'text-base/7 font-semibold']">
+            {{ tier.name }}</h3>
           <p class="mt-4 flex items-baseline gap-x-2">
-            <span :class="[tier.featured ? 'text-white' : 'text-gray-900', 'text-5xl font-semibold tracking-tight']">{{ tier.priceMonthly }}</span>
-            <span :class="[tier.featured ? 'text-gray-400' : 'text-gray-500', 'text-base']">/month</span>
+          <div v-show="tier.id != 'paquete-premium'">
+            <span :class="[tier.featured ? 'text-gray-400' : 'text-gray-500', 'text-base']">A partir de</span>
+          </div>
+          <span :class="[tier.featured ? 'text-white' : 'text-gray-900', 'text-5xl font-semibold tracking-tight']">{{
+            tier.priceMonthly }}</span>
           </p>
           <p :class="[tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-6 text-base/7']">{{ tier.description }}</p>
-          <ul role="list" :class="[tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-8 space-y-3 text-sm/6 sm:mt-10']">
+          <ul role="list"
+            :class="[tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-8 space-y-3 text-sm/6 sm:mt-10']">
             <li v-for="feature in tier.features" :key="feature.title" class="flex gap-x-3">
-              <UIcon :name="feature.uicon" class="w-6 h-6"/>
+              <UIcon :name="feature.uicon" class="w-6 h-6" />
               {{ feature.title }}
             </li>
           </ul>
           <div class="flex flex-row justify-center flex-wrap pt-4">
-            <CommonButtonsCtaContact/>
-            <CommonButtonsCtaCita/>
           </div>
         </div>
+
+      </div>
+      <div class="flex flex-row justify-center gap-8">
+        <CommonButtonsCtaContact />
+        <CommonButtonsCtaCita />
       </div>
     </div>
   </section>
@@ -36,32 +48,55 @@
 <script setup>
 const tiers = [
   {
-    name: 'Hobby',
-    id: 'tier-hobby',
+    name: 'Paquete Básico',
+    id: 'paquete-basico',
     href: '#',
-    priceMonthly: '$29',
-    description: "The perfect plan if you're just getting started with our product.",
+    priceMonthly: '€450',
+    description: "Ideal para pequeñas empresas que buscan establecer su presencia en línea con un sitio web profesional y funcional.",
     features: [
-      { title: '25 products', uicon: 'hugeicons:cursor-pointer-01' },
-      { title: 'Basic support', uicon: 'hugeicons:cursor-pointer-02' }
+      { title: 'Diseño de 1 a 10 páginas', uicon: 'hugeicons:browser-website' },
+      { title: 'Hospedaje y dominio incluidos', uicon: 'hugeicons:server-01' },
+      { title: 'Diseño responsive para móviles', uicon: 'hugeicons:mobile-phone-01' },
+      { title: 'Optimización SEO básica', uicon: 'hugeicons:search-magnifying-glass' },
+      { title: 'Formulario de contacto', uicon: 'hugeicons:mail-01' },
+      { title: 'Integración con redes sociales', uicon: 'hugeicons:share-01' },
     ],
     featured: false,
   },
   {
-    name: 'Enterprise',
-    id: 'tier-enterprise',
+    name: 'Paquete Profesional',
+    id: 'paquete-profesional',
     href: '#',
-    priceMonthly: '$99',
-    description: 'Dedicated support and infrastructure for your company.',
+    priceMonthly: '€850',
+    description: 'Perfecto para negocios en crecimiento que necesitan más que un sitio web estático para interactuar con sus clientes.',
     features: [
-      { title: 'Unlimited products', uicon: 'hugeicons:cursor-pointer-02' },
-      { title: 'Unlimited subscribers', uicon: 'hugeicons:cursor-pointer-02' },
-      { title: 'Advanced analytics', uicon: 'hugeicons:cursor-pointer-02' },
-      { title: 'Dedicated support representative', uicon: 'hugeicons:cursor-pointer-02' },
-      { title: 'Marketing automations', uicon: 'hugeicons:cursor-pointer-02' },
-      { title: 'Custom integrations', uicon: 'hugeicons:cursor-pointer-02' }
+      { title: 'Todo lo del Paquete Básico', uicon: 'hugeicons:check-circle-1' },
+      { title: 'Sistema de citas online', uicon: 'hugeicons:calendar-date' },
+      { title: 'Notificaciones por WhatsApp', uicon: 'hugeicons:whatsapp' },
+      { title: 'Galería de productos/servicios', uicon: 'hugeicons:image-multiple-01' },
+      { title: 'Automatización de marketing básica', uicon: 'hugeicons:automation-01' },
+      { title: 'Mejora la interacción con clientes', uicon: 'hugeicons:users-group-rounded' },
+      { title: 'Integración con CRM básico', uicon: 'hugeicons:hierarchy-3' },
+      { title: 'Optimización para motores de búsqueda avanzada', uicon: 'hugeicons:seo' },
+      { title: 'Soporte técnico prioritario', uicon: 'hugeicons:flag-06' },
     ],
     featured: true,
+  },
+  {
+    name: 'Paquete Premium',
+    id: 'paquete-premium',
+    href: '#',
+    priceMonthly: 'Consultar',
+    description: "Solución integral para empresas que requieren sistemas web complejos y altamente personalizados.",
+    features: [
+      { title: 'Desarrollo de sistemas web a medida', uicon: 'hugeicons:code-square-01' },
+      { title: 'Integración con sistemas existentes', uicon: 'hugeicons:puzzle-piece-01' },
+      { title: 'E-commerce (tiendas virtuales)', uicon: 'hugeicons:cart-shopping-fast' },
+      { title: 'Panel de administración personalizado', uicon: 'hugeicons:layout-dashboard-02' },
+      { title: 'Automatización de procesos empresariales', uicon: 'hugeicons:automation-02' },
+      { title: 'Escalabilidad para crecimiento futuro', uicon: 'hugeicons:rocket-02' },
+    ],
+    featured: false,
   },
 ]
 </script>
