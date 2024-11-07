d00498a0bc8f550fae22784ab022d2ea64977b12
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-11-07 10:41:18 +0100

    Link Button CTACita

--- components/common/buttons/CtaCita.vue
+++ components/common/buttons/CtaCita.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row">
     <UButton class="button mt-4 flex flex-row items-center gap-2 align-center self-center min-h-12"
-      to="https://calcom.titansdev.es/alexandre/cita" target="_blank">
+      to="https://calcom.titansdev.es/empezemos/cita" target="_blank">
       Agendar Cita Online
       <UIcon name="hugeicons:cursor-pointer-02" class="w-6 h-6 text-white" />
     </UButton>
