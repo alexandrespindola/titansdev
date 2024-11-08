57989a95f541ea33fbc86d519b9df36b40d00c73
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-11-08 22:19:47 +0100

    Aumenta o tempo limite de busca para 1500ms e atualiza as referências de configuração do Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -137,7 +137,7 @@ export default defineNuxtConfig({
     "@nuxtjs/strapi",
     "@nuxtjs/sitemap",
     "nuxt-schema-org",
-    "@nuxtjs/robots"
+    "@nuxtjs/robots",
   ],
   gtm: {
     id: "GTM-K5F3KFP3",
@@ -154,13 +154,15 @@ export default defineNuxtConfig({
     gzip: true,
     videos: [
       {
-        thumbnail_loc: 'https://res.cloudinary.com/xmfm6mhw3gwctndtcubgx2rptnae/video/upload/w_640,h_360,c_fill,q_auto,f_auto/so_0/video-notebook-streaming_jxwrof.jpg',
-        title: 'Video de Notebook',
-        description: 'Video de Notebook',
-        content_loc: 'https://res.cloudinary.com/xmfm6mhw3gwctndtcubgx2rptnae/video/upload/v1234567890/video-notebook-streaming_jxwrof.mp4',
-        player_loc: 'https://titansdev.com',
-        duration: '8',
-        publication_date: '2024-11-08',
+        thumbnail_loc:
+          "https://res.cloudinary.com/xmfm6mhw3gwctndtcubgx2rptnae/video/upload/w_640,h_360,c_fill,q_auto,f_auto/so_0/video-notebook-streaming_jxwrof.jpg",
+        title: "Video de Notebook",
+        description: "Video de Notebook",
+        content_loc:
+          "https://res.cloudinary.com/xmfm6mhw3gwctndtcubgx2rptnae/video/upload/v1234567890/video-notebook-streaming_jxwrof.mp4",
+        player_loc: "https://titansdev.com",
+        duration: "8",
+        publication_date: "2024-11-08",
       },
     ],
   },
@@ -173,7 +175,7 @@ export default defineNuxtConfig({
         output: {
           comments: false,
         },
-      }
+      },
     },
     css: {
       preprocessorOptions: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{vue,js}',
+    './components/**/*.{vue,js,ts}',
     './layouts/**/*.{vue,js}',
     './pages/**/*.{vue,js}',
     './plugins/**/*.{js,ts}',
