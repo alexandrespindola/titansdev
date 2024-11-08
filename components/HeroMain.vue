3a6bdb628fa0905793890901c6b4c78e2113c888
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-11-08 17:05:53 +0100

    Ajusta estilos no componente HeroMain e atualiza a fonte para 'Inter' no SCSS de tipografia

--- components/HeroMain.vue
+++ components/HeroMain.vue
@@ -54,7 +54,7 @@ useHead({
   left: 0;
   width: 100%;
   height: 100%;
-  object-fit: cover; /* Mantém a proporção da imagem */
+  object-fit: cover;
 }
 
 .startup-header::before {
@@ -68,6 +68,6 @@ useHead({
 }
 
 .container {
-  position: relative; /* Para garantir que o conteúdo fique acima da imagem */
+  position: relative;
 }
 </style>
