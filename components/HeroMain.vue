1df5887b4893553511b9e0b2b0e69acca5cee8fa
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-11-09 11:30:20 +0100

    Ajusta o estilo do título no componente HeroMain para melhorar a legibilidade, reduzindo o tamanho da fonte e alterando o estilo do subtítulo.

--- components/HeroMain.vue
+++ components/HeroMain.vue
@@ -8,9 +8,9 @@
         class="container h-screen flex flex-col justify-center p-4 md:max-w-2xl lg:max-w-4xl absolute left-1/2 transform -translate-x-1/2">
         <div class="flex flex-row">
           <div class="w-full flex flex-col text-center items-center gap-4">
-            <h1 class="text-3xl font-semibold uppercase md:text-4xl lg:text-5xl"
+            <h1 class="text-2xl font-semibold uppercase md:text-3xl lg:text-4xl"
               aria-label="Tecnologías modernas para su negocio">Tecnologías modernas para su negocio</h1>
-            <h2 class="flex flex-row text-2xl font-semibold">Soluciones asequibles para acelerar el crecimiento de
+            <h2 class="flex flex-row text-xl font-semibold">Soluciones asequibles para acelerar el crecimiento de
               pequeñas y medianas empresas</h2>
             <div class="flex flex-col md:flex-row gap-x-8">
               <CommonButtonsCtaContact />
