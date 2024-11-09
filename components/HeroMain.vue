f5573883ee40ce5e4d21800d41e7f50ab88c52db
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-11-09 11:46:28 +0100

    Atualiza o provedor de ícones para "server" e remove referências ao componente-meta, simplificando a configuração do projeto.

--- components/HeroMain.vue
+++ components/HeroMain.vue
@@ -8,9 +8,8 @@
         class="container h-screen flex flex-col justify-center p-4 md:max-w-2xl lg:max-w-4xl absolute left-1/2 transform -translate-x-1/2">
         <div class="flex flex-row">
           <div class="w-full flex flex-col text-center items-center gap-4">
-            <h1 class="text-2xl font-semibold uppercase md:text-3xl lg:text-4xl"
-              aria-label="Tecnologías modernas para su negocio">Tecnologías modernas para su negocio</h1>
-            <h2 class="flex flex-row text-xl font-semibold">Soluciones asequibles para acelerar el crecimiento de
+            <h1 class="hero" aria-label="Tecnologías modernas para su negocio">Tecnologías modernas para su negocio</h1>
+            <h2 class="flex flex-row text-lg font-semibold">Soluciones asequibles para acelerar el crecimiento de
               pequeñas y medianas empresas</h2>
             <div class="flex flex-col md:flex-row gap-x-8">
               <CommonButtonsCtaContact />
@@ -29,13 +28,20 @@ const { width } = useWindowSize()
 const isMobile = computed(() => width.value <= 768)
 </script>
 
-<!-- <style lang="scss">
+<style lang="scss">
 .hero {
-  font-weight: 600 !important;
-  font-size: 3rem;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  text-transform: uppercase;
 
-  @media screen and (max-width: 768px) {
-    font-size: 2.5rem;
+  @media screen and (min-width: 768px) {
+    font-size: 1.875rem;
+    line-height: 2.25rem
+  }
+
+  @media screen and (min-width: 1024px) {
+    font-size: 2.25rem;
+    line-height: 2.5rem
   }
 }
-</style> -->
+</style>
